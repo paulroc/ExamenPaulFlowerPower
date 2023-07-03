8d7f0a57ab86c239f2f783ed1a9c817914210ce4
paulroc <54845508+paulroc@users.noreply.github.com>
2023-07-03 10:51:32 +0200

    test

--- shop.php
+++ shop.php
@@ -27,7 +27,7 @@ if(isset($_POST['add_to_wishlist'])){
         $message[] = 'already added to cart';
     }else{
         mysqli_query($conn, "INSERT INTO `wishlist`(user_id, pid, name, price, image) VALUES('$user_id', '$product_id', '$product_name', '$product_price', '$product_image')") or die('query failed');
-        $message[] = 'product added to wishlist';
+        $message[] = 'product toegevoegd aan verlanglijst';
     }
 
 }
@@ -43,7 +43,7 @@ if(isset($_POST['add_to_cart'])){
     $check_cart_numbers = mysqli_query($conn, "SELECT * FROM `cart` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
 
     if(mysqli_num_rows($check_cart_numbers) > 0){
-        $message[] = 'already added to cart';
+        $message[] = 'al toegevoegd aan winkelwagen';
     }else{
 
         $check_wishlist_numbers = mysqli_query($conn, "SELECT * FROM `wishlist` WHERE name = '$product_name' AND user_id = '$user_id'") or die('query failed');
@@ -53,7 +53,7 @@ if(isset($_POST['add_to_cart'])){
         }
 
         mysqli_query($conn, "INSERT INTO `cart`(user_id, pid, name, price, quantity, image) VALUES('$user_id', '$product_id', '$product_name', '$product_price', '$product_quantity', '$product_image')") or die('query failed');
-        $message[] = 'product added to cart';
+        $message[] = 'product toegevoegd aan winkelwagen';
     }
 
 }
